d82796ddd74b844f517db88d124cd62e992aaa14
Elliot <hack00mind@gmail.com>
2023-01-15 16:20:18 +0800

    feat: mac clipboard event POC

---
+++ clipman/src/main.rs
@@ -0,0 +1,46 @@
+use objc2::foundation::{NSArray, NSDictionary, NSObject, NSString};
+use objc2::rc::{Id, Shared};
+use objc2::runtime::{Class, Object};
+use objc2::{msg_send, class};
+
+use std::{thread, time};
+
+#[link(name = "AppKit", kind = "framework")]
+extern "C" {}
+
+fn main() {
+    let cls = Class::get("NSPasteboard").unwrap();
+
+    let pasteboard: *mut Object = unsafe { msg_send![cls, generalPasteboard] };
+
+    let mut cur_count: isize = unsafe { msg_send![pasteboard, changeCount] };
+
+    loop {
+        let count: isize = unsafe { msg_send![pasteboard, changeCount] };
+
+        if cur_count == count {
+            thread::sleep(time::Duration::from_millis(500));
+            continue;
+        }
+
+        let string_class = class!(NSString);
+
+        let classes: Id<NSArray<NSObject, Shared>, Shared> = unsafe {
+            let array = msg_send![class!(NSArray), arrayWithObject:string_class];
+            Id::new(array).unwrap()
+        };
+
+        let options: Id<NSDictionary<NSObject, NSObject>, Shared> = NSDictionary::new();
+        let string_array: Id<NSArray<NSString, Shared>, Shared> = unsafe {
+            let obj: *mut NSArray<NSString, Shared> = msg_send![pasteboard, readObjectsForClasses:&*classes options:&*options];
+            Id::new(obj)
+                .unwrap()
+        };
+
+        if string_array.len() > 0 {
+            println!("{:?}", string_array)
+        }
+
+        cur_count = count;
+    }
+}
